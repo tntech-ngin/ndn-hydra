ff464e8c0b6ce33b0a7c85547506e0d8fbc6f302
tntech-ngin <54043715+tntech-ngin@users.noreply.github.com>
2024-03-04 22:57:33 -0600

    Update favor_calculator.py

--- ndn_hydra/repo/modules/favor_calculator.py
+++ ndn_hydra/repo/modules/favor_calculator.py
@@ -1,27 +1,22 @@
-# -------------------------------------------------------------
-# NDN Hydra Favor Calculator
-# -------------------------------------------------------------
 #  @Project: NDN Hydra
-#  @Date:    2021-01-25
+#  @Date:    2024-03-04
 #  @Authors: Please check AUTHORS.rst
 #  @Source-Code:   https://github.com/justincpresley/ndn-hydra
 #  @Documentation: https://ndn-hydra.readthedocs.io
 #  @Pip-Library:   https://pypi.org/project/ndn-hydra
 # -------------------------------------------------------------
-
 import logging
 import numpy as np
 from ndn.encoding import *
-from os import shutil
-
+import shutil
 
 class FavorParameterTypes:
-#    RTT = 100
+    RTT = 100
     NETWORK_COST_PER_GB = 0.01
-    STOARGE_COST_PER_GB = 0.014
+    STORAGE_COST_PER_GB = 0.014
     NUM_USERS = 100
     BANDWIDTH = 25000 #Mbps
-    NETWORK_COST = COST_PER_GB * (BANDWIDTH/(1000*8)) #0.01 USD/GB  
+    NETWORK_COST = NETWORK_COST_PER_GB * (BANDWIDTH/(1000*8)) #0.01 USD/GB  
     RW_SPEED = 6.25
     TOTAL_STORAGE, USED_STORAGE, REMAINING_STORAGE = shutil.disk_usage(__file__)
     STORAGE_COST = REMAINING_STORAGE * STORAGE_COST_PER_GB
@@ -33,7 +28,7 @@ class FavorParameters(TlvModel):
     network_cost = BytesField(FavorParameterTypes.NETWORK_COST)
     storage_cost = BytesField(FavorParameterTypes.STORAGE_COST)
     remaining_storage = BytesField(FavorParameterTypes.REMAINING_STORAGE)
-    
+
 
 class FavorCalculator:
     """
@@ -47,6 +42,3 @@ class FavorCalculator:
         # print('favor:', favor)
         favor = .3*REMAINING_STORAGE + .3*BANDWIDTH + .4*RW_SPEED + 0.0*NUM_USERS + 0.0*NETWORK_COST + 0.0*STORAGE_COST
         return favor
-
-
-    
