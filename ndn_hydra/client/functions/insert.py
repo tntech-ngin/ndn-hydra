2246f29eb4d41a929166ded13a26be1800489d0f
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2024-06-03 22:05:57 -0500

    [FEAT] Add a check to see if the file name already exists before file insertion

--- ndn_hydra/client/functions/insert.py
+++ ndn_hydra/client/functions/insert.py
@@ -16,67 +16,77 @@ from ndn.app import NDNApp
 from ndn.encoding import Name, Component, FormalName
 from ndn_hydra.repo.protocol.base_models import InsertCommand, File
 from ndn_hydra.repo.utils.pubsub import PubSub
+from ndn_hydra.client.functions.fetch import HydraFetchClient
 
 SEGMENT_SIZE = 8192
 
+
 class HydraInsertClient(object):
     def __init__(self, app: NDNApp, client_prefix: FormalName, repo_prefix: FormalName) -> None:
-      """
-      This client inserts data packets from the remote repo.
-      :param app: NDNApp.
-      :param client_prefix: NonStrictName. Routable name to client.
-      :param repo_prefix: NonStrictName. Routable name to remote repo.
-      """
-      self.app = app
-      self.client_prefix = client_prefix
-      self.repo_prefix = repo_prefix
-      self.pb = PubSub(self.app, self.client_prefix)
-      self.packets = []
+        """
+        This client inserts data packets from the remote repo.
+        :param app: NDNApp.
+        :param client_prefix: NonStrictName. Routable name to client.
+        :param repo_prefix: NonStrictName. Routable name to remote repo.
+        """
+        self.app = app
+        self.client_prefix = client_prefix
+        self.repo_prefix = repo_prefix
+        self.pb = PubSub(self.app, self.client_prefix)
+        self.packets = []
 
     async def insert_file(self, file_name: FormalName, path: str) -> bool:
-      """
-      Insert a file associated with a file name to the remote repo
-      """
-      size, seg_cnt = 0, 0
-      fetch_file_prefix = self.client_prefix + [Component.from_str("upload")] + file_name
-      tic = time.perf_counter()
-      with open(path, "rb") as f:
-        data = f.read()
-        size = len(data)
-        seg_cnt = (len(data) + SEGMENT_SIZE - 1) // SEGMENT_SIZE
-        self.packets = [self.app.prepare_data(fetch_file_prefix + [Component.from_segment(i)],
-                                              data[i*SEGMENT_SIZE:(i+1)*SEGMENT_SIZE],
-                                              freshness_period=10000,
-                                              final_block_id=Component.from_segment(seg_cnt - 1))
-                        for i in range(seg_cnt)]
+        """
+        Insert a file associated with a file name to the remote repo
+        """
+        size, seg_cnt = 0, 0
+        fetch_file_prefix = self.client_prefix + [Component.from_str("upload")] + file_name
 
-      print(f'Created {seg_cnt} chunks under name {Name.to_str(fetch_file_prefix)}')
+        # Check if the file name already exists
+        fetch_client = HydraFetchClient(self.app, self.client_prefix, self.repo_prefix)
+        local_file_name = fetch_client.fetch_file(file_name)
 
-      def on_interest(int_name, _int_param, _app_param):
-        seg_no = Component.to_number(int_name[-1]) if Component.get_type(int_name[-1]) == Component.TYPE_SEGMENT else 0
-        if seg_no < seg_cnt:
-            self.app.put_raw_packet(self.packets[seg_no])
-        if seg_no == (seg_cnt - 1):
-            toc = time.perf_counter()
-            print(f"The publication is complete! - total time (with disk): {toc - tic:0.4f} secs")
+        if not local_file_name == file_name:
+            tic = time.perf_counter()
+            with open(path, "rb") as f:
+                data = f.read()
+                size = len(data)
+                seg_cnt = (len(data) + SEGMENT_SIZE - 1) // SEGMENT_SIZE
+                self.packets = [self.app.prepare_data(fetch_file_prefix + [Component.from_segment(i)],
+                                                      data[i*SEGMENT_SIZE:(i+1)*SEGMENT_SIZE],
+                                                      freshness_period=10000,
+                                                      final_block_id=Component.from_segment(seg_cnt - 1))
+                                for i in range(seg_cnt)]
 
-      self.app.route(fetch_file_prefix)(on_interest)
+            print(f'\nCreated {seg_cnt} chunks under name {Name.to_str(fetch_file_prefix)}')
+        else:
+            print(f'\nFile {file_name} already exists')
 
-      file = File()
-      file.file_name = file_name
-      file.packets = seg_cnt
-      file.packet_size = SEGMENT_SIZE
-      file.size = size
-      cmd = InsertCommand()
-      cmd.file = file
-      cmd.fetch_path = fetch_file_prefix
-      cmd_bytes = cmd.encode()
+        def on_interest(int_name, _int_param, _app_param):
+            seg_no = Component.to_number(int_name[-1]) if Component.get_type(int_name[-1]) == Component.TYPE_SEGMENT else 0
+            if seg_no < seg_cnt:
+                self.app.put_raw_packet(self.packets[seg_no])
+            if seg_no == (seg_cnt - 1):
+                toc = time.perf_counter()
+                print(f"The publication is complete! - total time (with disk): {toc - tic:0.4f} secs")
 
-      # publish msg to repo's insert topic
-      await self.pb.wait_for_ready()
-      is_success = await self.pb.publish(self.repo_prefix + ['insert'], cmd_bytes)
-      if is_success:
-          logging.debug('Published an insert msg and was acknowledged by a subscriber')
-      else:
-          logging.debug('Published an insert msg but was not acknowledged by a subscriber')
-      return is_success
+        self.app.route(fetch_file_prefix)(on_interest)
+
+        file = File()
+        file.file_name = file_name
+        file.packets = seg_cnt
+        file.packet_size = SEGMENT_SIZE
+        file.size = size
+        cmd = InsertCommand()
+        cmd.file = file
+        cmd.fetch_path = fetch_file_prefix
+        cmd_bytes = cmd.encode()
+
+        # publish msg to repo's insert topic
+        await self.pb.wait_for_ready()
+        is_success = await self.pb.publish(self.repo_prefix + ['insert'], cmd_bytes)
+        if is_success:
+            logging.debug('Published an insert msg and was acknowledged by a subscriber')
+        else:
+            logging.debug('Published an insert msg but was not acknowledged by a subscriber')
+        return is_success
