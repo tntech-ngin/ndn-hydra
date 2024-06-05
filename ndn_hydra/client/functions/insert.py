eba79d22a75cc106e2f18c0f335dd3eee9499685
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2024-06-05 09:06:01 -0500

    [WIP] Fix query name 4

--- ndn_hydra/client/functions/insert.py
+++ ndn_hydra/client/functions/insert.py
@@ -44,7 +44,7 @@ class HydraInsertClient(object):
 
         # Check if the file name already exists
         query_client = HydraQueryClient(self.app, self.client_prefix, self.repo_prefix)
-        query = self.client_prefix + [Component.from_str("query")] + file_name
+        query = self.client_prefix + [Component.from_str("file")] + file_name
 
         local_file_name = await query_client.send_query(query)
 
