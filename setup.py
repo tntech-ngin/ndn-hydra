e095b28c3ccf4335fc7fab0ddc6f9455d495d38f
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2024-06-21 09:44:08 -0500

    [WIP] Increase pacth version to 0.3.18

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ import re
 from setuptools import setup, find_packages
 from typing import List
 
+
 with io.open("./docs/version.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
