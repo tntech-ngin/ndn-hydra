0c3910116ea4480ada0386b549f1797cff45131b
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2024-06-30 16:23:16 -0500

    [FIX] Supress uneseful logs 9

--- ndn_hydra/repo/main/main.py
+++ ndn_hydra/repo/main/main.py
@@ -181,7 +181,7 @@ class HydraNodeThread(Thread):
                 pass
 
         # logging
-        SVSyncLogger.config(False, None, logging.CRITICAL)
+        # SVSyncLogger.config(False, None, logging.CRITICAL)
 
         log_level = getattr(logging, self.config['logger_level'].upper(), logging.INFO)
 
@@ -192,12 +192,6 @@ class HydraNodeThread(Thread):
         console = logging.StreamHandler()
         console.setLevel(log_level)
         logging.getLogger().addHandler(console)
-
-        sv_logger = logging.getLogger('SVSyncLogger')
-        if log_level == "CRITICAL":
-            sv_logger.setLevel(logging.CRITICAL)
-        else:
-            sv_logger.setLevel(logging.CRITICAL)
 
         # NDN
         app = NDNApp()
