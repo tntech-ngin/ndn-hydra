aae5c377a68551c196f35a18a9bc616702c40c5e
Sankalpa Timilsina <sankalpatimilsina12@gmail.com>
2024-07-16 15:52:25 -0500

    Minor: Style changes, remove redundancy

--- ndn_hydra/repo/modules/data_storage.py
+++ ndn_hydra/repo/modules/data_storage.py
@@ -17,7 +17,7 @@ class DataStorage(SqliteStorage):
 
     def _prepare_cache(self, file_name: str, total_segments: int, can_be_prefix: bool = False, must_be_fresh: bool = False) -> None:
         try: 
-            all_keys = [file_name + "/seg=" + str(i) for i in range(total_segments)]
+            all_keys = [file_name + '/seg=' + str(i) for i in range(total_segments)]
             batch_keys_b = [self._get_name_bytes_wo_tl(Name.normalize(key)) for key in all_keys]
             cursor = self.conn.cursor()
             current_time = time.time()
@@ -34,20 +34,19 @@ class DataStorage(SqliteStorage):
                 if can_be_prefix:
                     conditions = ' OR '.join(['hex(key) LIKE ?'] * len(batch_keys_b_part))
                     order_by = 'CASE key ' + ' '.join([f'WHEN hex(key) LIKE ? THEN {j}' for j in range(len(batch_keys_b_part))]) + ' END'
-                    query = f"{query_base}{conditions} ORDER BY {order_by}"
                     params = tuple(key.hex() + '%' for key in batch_keys_b_part) + tuple(key.hex() + '%' for key in batch_keys_b_part)
                 else:
                     conditions = 'key IN ({})'.format(', '.join('?' * len(batch_keys_b_part)))
                     order_by = 'CASE key ' + ' '.join([f'WHEN key = ? THEN {j}' for j in range(len(batch_keys_b_part))]) + ' END'
-                    query = f"{query_base}{conditions} ORDER BY {order_by}"
                     params = tuple(batch_keys_b_part) + tuple(batch_keys_b_part)
+                query = f'{query_base}{conditions} ORDER BY {order_by}'
                 cursor.execute(query, params)
 
                 for j, row in enumerate(cursor.fetchall()):
                     self.db_cache[batch_keys[j]] = (row[0] if row else None, current_time + self.db_cache_timeout)
 
         except sqlite3.Error as e:
-            print(f"SQLite error: {e}")
+            print(f'SQLite error: {e}')
         finally:
             cursor.close()
 
@@ -60,7 +59,7 @@ class DataStorage(SqliteStorage):
             return val[0]
 
         # Prepare cache when segment 0 is requested
-        if segment_comp == "/seg=0":
+        if segment_comp == '/seg=0':
             # TODO: Prepare cache only if we have sufficient memory in the system
             self._prepare_cache(file_name, total_segments, can_be_prefix, must_be_fresh)
             # aio.get_event_loop().run_in_executor(None, self._prepare_cache, file_name, total_segments, can_be_prefix, must_be_fresh)
@@ -76,7 +75,7 @@ class DataStorage(SqliteStorage):
 
     def remove_packets(self, names: List[NonStrictName]) -> int:
         if not self.initialized:
-            raise self.UninitializedError("The storage is not initialized.")
+            raise self.UninitializedError('The storage is not initialized.')
 
         keys = [self._get_name_bytes_wo_tl(Name.normalize(name)) for name in names]
         if not keys:
@@ -100,7 +99,7 @@ class DataStorage(SqliteStorage):
 
         except sqlite3.Error as e:
             self.conn.rollback()
-            print(f"SQLite error: {e}")
+            print(f'SQLite error: {e}')
 
         finally:
             if c: c.close()
@@ -109,6 +108,6 @@ class DataStorage(SqliteStorage):
             self.conn.execute('VACUUM')
 
         except sqlite3.Error as e:
-            print(f"SQLite error during VACUUM: {e}")
+            print(f'SQLite error during VACUUM: {e}')
 
         return total_deleted
